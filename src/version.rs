390c7e0ff7fdf79b13384ab0d162bf65ab0c60a7
Zachary Sims <zac.sims@gmail.com>
2018-06-12 10:31:50 +1000

    Adds support for reading the clamav version

--- src/version.rs
+++ src/version.rs
@@ -5,12 +5,18 @@ use ffi;
 
 /// Returns the database version level that the engine supports
 pub fn flevel() -> u32 {
-    unsafe {
-        ffi::cl_retflevel()
-    }
+    unsafe { ffi::cl_retflevel() }
 }
 
 /// Gets the clamav engine version
+///
+/// # Example
+///
+/// ```
+/// use clamav::{version};
+///
+/// println!("Running version {} flevel {}", version::version(), version::flevel());
+/// ```
 pub fn version() -> String {
     unsafe {
         let ptr = ffi::cl_retver();
@@ -19,5 +25,22 @@ pub fn version() -> String {
             .ok()
             .expect("Invalid UTF8 string")
             .to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn version_success() {
+        ::initialize().expect("initialize should succeed");
+        assert!(version().len() > 0, "expected a version");
+    }
+
+    #[test]
+    fn flevel_success() {
+        ::initialize().expect("initialize should succeed");
+        assert!(flevel() > 0, "expected an flevel");
     }
 }
